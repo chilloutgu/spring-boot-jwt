c10909b0aa9cee0051586ce72e2a70412f15c661
chillgu <bbonggu92@gmail.com>
2020-04-09 21:06:41 +0900

    init

---
+++ src/test/java/me/chillgu/jwt/Jwt02ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chillgu.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
